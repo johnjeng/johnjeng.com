7643c5346e09f190fafdc5bf313e4ea4041a24f3
John Jeng <j3@sagemath.com>
2017-12-08 12:58:29 -0800

    Add social media buttons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,26 +1,43 @@
 import React from "react"
 import Link from "gatsby-link"
+import Icon from "react-fontawesome"
+import Helmet from "react-helmet";
 
 const ListLink = props =>
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <li style={{ display: `inline-block`, marginRight: `1rem`, marginBottom: `0px`}}>
     <Link to={props.to}>
       {props.children}
     </Link>
   </li>
 
+const IconLink = props =>
+  <li style={{ display: `inline-block`, marginRight: `1rem`, marginBottom: `0px` }}>
+    <a href={props.to} target="_blank" role="button" >
+      <Icon name={props.name} />
+    </a>
+  </li>
+
+
 export default ({ children, data }) => (
   <div style={{ margin: `0 auto`, maxWidth: 800, padding: `0 1rem` }}>
+    <Helmet>
+      <script src="https://use.fontawesome.com/be0931746d.js"></script>
+    </Helmet>
     <header style={{ margin: `2rem 0` }}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
         <h3 style={{ display: `inline` }}>
           {data.site.siteMetadata.title}
         </h3>
       </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
+      <ul style={{ listStyle: `none`, float: `right`, margin:`0px` }}>
         <ListLink to="/">Home</ListLink>
         <ListLink to="/about/">About</ListLink>
         <ListLink to="/recent-posts/">Blog Posts</ListLink>
         <ListLink to="/contact/">Contact</ListLink>
+        <IconLink to="mailto:john.jeng5@gmail.com" name="envelope" />
+        <IconLink to="https://facebook.com/johnjeffjeng" name="facebook" />
+        <IconLink to="https://instagram.com/johnjeffjeng/" name="instagram" />
+        <IconLink to="https://m.me/johnjeffjeng" name="message" />
       </ul>
     </header>
     {children()}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from "react"
 
 import Link from "gatsby-link"
 
-export default ({ data }) =>
+export default () =>
   <div>
-    This website is under construction using gatsby.js
+    This website is under construction using
+    {" "}<a href="https://www.gatsbyjs.org/">gatsbyjs</a>.
   </div>
